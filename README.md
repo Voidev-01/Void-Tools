7806ae80c0444cb071361803593209b381a38166
voidev-01 <voidevvoidev@gmail.com>
2026-07-02 11:55:33 +0330

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+## License
+
+This project is licensed under the Void-Tools Source-Available License (VTSAL).
+
+See the LICENSE file for complete terms.
+
+
+
+
---
+++ src/main.py
@@ -0,0 +1,33 @@
+"""
+Copyright (c) 2026 Voidev
+
+This file is part of Void-Tools.
+
+Licensed under the Void-Tools Source-Available License (VTSAL).
+See the LICENSE file for details.
+"""
+# ============================================================================================= #
+# ========== call_Moduls========== #
+import json
+import datetime
+import os
+import sys
+import random
+import logging
+import pathlib
+import math
+import socket
+import threading
+from PySide6.QtWidgets import (QApplication,QMainWindow,QWidget,
+QLineEdit,QLabel,QLayout,QBoxLayout,QTabBar,QStatusBar)
+from PySide6.QtCore import Qt,QUrl
+from PySide6.QtGui import QAction,QIcon
+from PySide6.QtWebEngineWidgets import QWebEngineView
+import qtawesome as qta
+import requests
+import bs4
+import django
+import pygame
+# ====================================================== #
+
+
